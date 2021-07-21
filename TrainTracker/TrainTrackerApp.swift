c242777bb7e01be3c3b2c06f9811ea5d5ff2e4c1
nickrepaci <nickrepaci@gmail.com>
2021-07-20 21:59:18 -0400

    test second commit

--- TrainTracker/TrainTrackerApp.swift
+++ TrainTracker/TrainTrackerApp.swift
@@ -15,6 +15,7 @@ struct TrainTrackerApp: App {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            
         }
     }
 }
