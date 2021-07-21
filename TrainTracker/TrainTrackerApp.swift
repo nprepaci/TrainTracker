e6b4d732885e195b27b62eb4a4f21bc463b028aa
nickrepaci <nickrepaci@gmail.com>
2021-07-20 21:58:13 -0400

    first commit

---
+++ TrainTracker/TrainTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TrainTrackerApp.swift
+//  TrainTracker
+//
+//  Created by Nicholas Repaci on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TrainTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
