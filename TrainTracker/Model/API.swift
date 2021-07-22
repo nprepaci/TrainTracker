ad7bcd17107effdebac0778520b0d7b09180219b
nickrepaci <nickrepaci@gmail.com>
2021-07-22 01:20:14 -0400

    built accurate model for API. Now need to publish data

--- TrainTracker/Model/API.swift
+++ TrainTracker/Model/API.swift
@@ -5,29 +5,24 @@
 //  Created by Nicholas Repaci on 7/20/21.
 //
 
-import SwiftUI
-
 import Foundation
 import SwiftUI
 
-struct MealResponse: Codable {
-    var meals: [[String: String?]]
+
+struct GTFSObject: Codable {
+    let data: [TrainResponse]
+    let updated: String
 }
 
-//struct Result: Codable {
-//    let arrivals: [String]
-//    let lat, lon, name: String
-//}
-
 struct TrainResponse: Codable {
-    let n, s: [N]
-    let id: String
-    let lastUpdate: Date
-    let location: [Double]
-    let name: String
-    let routes: [String]
-    let stops: [String: [Double]]
-
+    let n, s: [NS]?
+    let id: String?
+    let lastUpdate: String?
+    let location: [Double]?
+    let name: String?
+    let routes: [String]?
+    let stops: [String: [Double]]?
+    
     enum CodingKeys: String, CodingKey {
         case n = "N"
         case s = "S"
@@ -37,43 +32,145 @@ struct TrainResponse: Codable {
     }
 }
 
-// MARK: - N
-struct N: Codable {
-    let route: String
-    let time: Date
+struct NS: Codable {
+    let route: String?
+    let time: String?
 }
 
 class API: ObservableObject {
     
-    @Published var arrivals = [String]()
-    @Published var lat = String()
-    @Published var lon = String()
-    @Published var name = String()
+    @Published var data: TrainResponse?
+    @Published var updated = String()
     
     func loadData() {
-        guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/random.php") else {
+        guard let url = URL(string: "http://127.0.0.1:5000/by-route/A") else {
             print("Your API end point is Invalid")
             return
         }
         let request = URLRequest(url: url)
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data {
-                if let response = try? JSONDecoder().decode(TrainResponse.self, from: data) {
+                if let response = try? JSONDecoder().decode(GTFSObject.self, from: data) {
+                    //print("\n-------> response: \(response)\n")
                     DispatchQueue.main.async {
-                        if let firstOne = response.meals.first {
-                            self.meals = response.meals
-                            self.firstMeal = firstOne
-                            self.mealName = (self.firstMeal["strMeal"] ?? "") ?? ""
-                            self.mealInstructions = (self.firstMeal["strInstructions"] ?? "") ?? ""
+                        if let data = response.data.first {
+                            self.data = data
+                            self.updated = response.updated
+                        
                         }
+                        return
                     }
-                    return
                 }
             }
         }.resume()
     }
 }
 
+//struct Main: Codable {
+//    let data: [Datum]
+//    let updated: String
+//}
+//
+//// MARK: - Datum
+//struct Datum: Codable {
+//    let n, s: [N]
+//    let id: String
+//    let lastUpdate: Date
+//    let location: [Double]
+//    let name: String
+//    let routes: [String]
+//    let stops: [String: [Double]]
+//
+//    enum CodingKeys: String, CodingKey {
+//        case n = "N"
+//        case s = "S"
+//        case id
+//        case lastUpdate = "last_update"
+//        case location, name, routes, stops
+//    }
+//}
+//
+//// MARK: - N
+//struct N: Codable {
+//    let route: String
+//    let time: Date
+//}
+//
+//class API: ObservableObject {
+//
+//    @Published var data: Datum?
+//    @Published var updated = String()
+//
+//    @Published var storedData = [String: String?]()
+//    //@Published var testN =
+//    //@Published whatever =
+//    @Published var testRoute = String()
+//
+//    @Published var ns = N.self
+//    @Published var id = String()
+//    @Published var lastUpdate = Date()
+//    @Published var location = [Double]()
+//    @Published var name = String()
+//    @Published var routes = [String]()
+//    @Published var stops = [String: [Double]]()
+//
+//    func loadData() {
+//        guard let url = URL(string: "http://127.0.0.1:5000/by-route/A") else {
+//            print("Your API end point is Invalid")
+//            return
+//        }
+//        let request = URLRequest(url: url)
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let response = try? JSONDecoder().decode(Main.self, from: data) {
+//                    DispatchQueue.main.async {
+//                        if let data = response.data.first {
+//                            self.data = data
+//                            self.updated = response.updated
+//
+////                            self.routes = response.routes
+////                            self.id = response.id
+////                            self.lastUpdate = response.lastUpdate
+////                            self.location = response.location
+////                            self.name = response.name
+////                            self.routes = response.routes
+////                            self.stops = response.stops
+//                        }
+//                    }
+//                    return
+//                }
+//            }
+//        }.resume()
+//    }
+//}
+
+
+//struct MealResponse: Codable {
+//    var meals: [[String: String?]]
+//}
+//
+//class API: ObservableObject {
+//
+//    @Published var meals = [[String: String?]]()
+//    @Published var firstMeal = [String: String?]()
+//    @Published var mealName = String()
+//    @Published var mealInstructions = String()
+//
+//    func loadData() {
+//        guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/random.php") else {
+//            print("Your API end point is Invalid")
+//            return
+//        }
+//        let request = URLRequest(url: url)
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let response = try? JSONDecoder().decode(MealResponse.self, from: data) {
+//                    DispatchQueue.main.async {
+//                        if let firstOne = response.meals.first {
+//                            self.meals = response.meals
+//                            self.firstMeal = firstOne
+//                            self.mealName = (self.firstMeal["strMeal"] ?? "") ?? ""
+//                            self.mealInstructions = (self.firstMeal["strInstructions"] ?? "") ?? ""
 
 
 //Sample Data:
@@ -120,3 +217,79 @@ class API: ObservableObject {
 //                ]
 //            }
 //        },
+
+
+
+
+//
+//struct TrainResponse: Codable {
+//    let data: [[String: String?]]
+//    let n, s: [NS]
+//    let id: String
+//    let lastUpdate: Date
+//    let location: [Double]
+//    let name: String
+//    let routes: [String]
+//    let stops: [String: [Double]]
+//
+//    enum CodingKeys: String, CodingKey {
+//        case data
+//        case n = "N"
+//        case s = "S"
+//        case id
+//        case lastUpdate = "last_update"
+//        case location, name, routes, stops
+//    }
+//}
+//
+//// MARK: - N
+//struct NS: Codable {
+//    let route: String
+//    let time: Date
+//}
+
+
+//class API: ObservableObject {
+//
+//    @Published var data = [Datum]()
+//
+//    @Published var storedData = [String: String?]()
+//    //@Published var testN =
+//    //@Published whatever =
+//    @Published var testRoute = String()
+//
+//    @Published var ns = N.self
+//    @Published var id = String()
+//    @Published var lastUpdate = Date()
+//    @Published var location = [Double]()
+//    @Published var name = String()
+//    @Published var routes = [String]()
+//    @Published var stops = [String: [Double]]()
+//
+//    func loadData() {
+//        guard let url = URL(string: "http://127.0.0.1:5000/by-route/A") else {
+//            print("Your API end point is Invalid")
+//            return
+//        }
+//        let request = URLRequest(url: url)
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let response = try? JSONDecoder().decode(TrainResponse.self, from: data) {
+//                    DispatchQueue.main.async {
+//                        if let data = response.data.first {
+//                            self.storedData = data.self
+//
+//                            self.routes = response.routes
+//                            self.id = response.id
+//                            self.lastUpdate = response.lastUpdate
+//                            self.location = response.location
+//                            self.name = response.name
+//                            self.routes = response.routes
+//                            self.stops = response.stops
+//                        }
+//                    }
+//                    return
+//                }
+//            }
+//        }.resume()
+//    }
