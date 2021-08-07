ebb3be02c74a28247f9cfe087c041149fe7c627c
nickrepaci <nickrepaci@gmail.com>
2021-08-07 14:25:21 -0400

    Added unit test for API

--- TrainTrackerTests/TrainTrackerTests.swift
+++ TrainTrackerTests/TrainTrackerTests.swift
@@ -9,6 +9,9 @@ import XCTest
 @testable import TrainTracker
 
 class TrainTrackerTests: XCTestCase {
+    
+    var api = API()
+    
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,10 +20,31 @@ class TrainTrackerTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testAPI() {
+        
+        // Create an expectation for a background download task.
+        let expectation = XCTestExpectation(description: "Test if API is returning data. Be sure to launch server before testing.")
+        
+        // Create a URL for a web page to be downloaded.
+        let url = URL(string: "http://127.0.0.1:5000/by-route/A")!
+        
+        // Create a background task to download the web page.
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+            
+            // Make sure we downloaded some data.
+            XCTAssertNotNil(data, "No data was downloaded.")
+            
+            // Fulfill the expectation to indicate that the background task has finished successfully.
+            expectation.fulfill()
+            
+        }
+        
+        // Start the download task.
+        dataTask.resume()
+        
+        // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+        wait(for: [expectation], timeout: 10.0)
     }
 
     func testPerformanceExample() throws {
